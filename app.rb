e97f336a753e4c471db789f7386316f8d19f2381
Ergonomisk <erik.ovukaandersson@elev.ga.ntig.se>
2025-09-11 16:40:01 +0200

    made a website

--- app.rb
+++ app.rb
@@ -1,1 +1,32 @@
 require 'sinatra'
+require 'slim'
+require 'sinatra/reloader'
+
+get ('/') do
+  @banandata="erik"
+  slim(:start)
+end
+
+get ('/home') do
+  slim(:home)
+end
+
+get ('/fruits') do
+  @fruits =["äpple", "banan", "melon"]
+  slim(:fruits)
+end
+
+get ('/about') do
+  slim(:about)
+end
+
+get ('/fruits/:id')do
+  fruits =["äpple", "banan", "melon"]
+  id = params[:id].to_i
+  @fruits =fruits[id]
+  slim(:visafrukt)
+
+end
+
+
+
